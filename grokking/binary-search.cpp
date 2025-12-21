d55378418313f0298698b6d7028e4fad2a4431a7
padre_paulas <pounowyou@gmail.com>
2025-12-22 00:42:54 +0200

    refactor, put the biSearch in a separate function

--- grokking/binary-search.cpp
+++ grokking/binary-search.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 #include <iterator>
 
+int biSearch(int arr[], int arrSize, int lookingFor);
+
 int main() {
 
   int arr[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-
   int lookingFor = 20;
-  int right = sizeof(arr) / sizeof(arr[0]) - 1;
+  int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+  int i = biSearch(arr, arrSize, lookingFor);
+
+  if (i != -1) {
+    std::cout << "Value found at i: " << i << std::endl;
+  }
+
+  return 0;
+}
+
+int biSearch(int arr[], int arrSize, int lookingFor) {
+  int right = arrSize - 1;
   int left = 0;
   int mid = 0;
 
   while (left <= right) {
     mid = (right + left) / 2;
     if (arr[mid] == lookingFor) {
-      std::cout << "Value found at i: " << mid << std::endl;
-      break;
+      return mid;
     }
     if (arr[mid] > lookingFor) {
       std::cout << "right: " << right << " left: " << left << " mid: " << mid << std::endl;
@@ -24,7 +36,5 @@ int main() {
       left = mid + 1;
     }
   }
-
-
-  return 0;
+  return -1;
 }
