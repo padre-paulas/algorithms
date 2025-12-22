b7c4f6ec09009f22698919a4945f301ffcacc42e
padre_paulas <pounowyou@gmail.com>
2025-12-22 17:43:30 +0200

    read file create file-read-2.cpp,

--- C-and-C++/file-io/file-read-1.cpp
+++ C-and-C++/file-io/file-read-1.cpp
@@ -6,5 +6,23 @@ int main() {
   std::ifstream inFile;
   inFile.open("file-1.txt");
 
+  if (!inFile.is_open()) {
+    std::cout << "Couldn't open the file\n";
+    return 1;
+  }
+
+  int number = 0;
+
+  inFile >> number;
+  inFile >> number;
+  if(inFile.fail()) {
+    std::cout << "Error file format incorrect.\n";
+    return 1;
+  }
+
+  std::cout << number << std::endl;
+
+  inFile.close();
+
   return 0;
 }
