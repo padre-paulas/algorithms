1ba71cc1666fdf8f4563af2b29b57cf46bc07d71
padre_paulas <pounowyou@gmail.com>
2025-12-22 16:42:29 +0200

    create swap function with passing by pointer

--- C-and-C++/pointers-2.c
+++ C-and-C++/pointers-2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void swap(int *a, int *b);
+
 int main() {
 
   int a, b, c;
@@ -10,5 +12,21 @@ int main() {
 
   printf("Result: %d\n", a + b + c);
 
+  int x = 5;
+  int y = 10;
+
+  int *p1 = &x;
+  int *p2 = &y;
+
+  swap(&x, &y);
+  swap(p1, p2);
+
   return 0;
+}
+
+void swap(int *a, int *b) {
+  int temp = 0;
+  temp = *a;
+  *a = *b;
+  *b = temp;
 }
