3141471be50e24c7dedefc7358b2b2622a25f868
padre_paulas <pounowyou@gmail.com>
2025-12-24 00:01:38 +0200

    create bank account class

---
+++ C-and-C++/structs-classes/class-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+class BankAccount {
+  public:
+    string name;
+    int balance;
+
+  private:
+    string lastName;
+};
+
+int main() {
+
+
+  return 0;
+}
+
+void first() {
+
+ BankAccount newAccount;
+
+ newAccount.name;
+
+}
