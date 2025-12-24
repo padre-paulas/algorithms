2939cbd5c419b92cb9febb81085e2fad8d9c2b77
padre_paulas <pounowyou@gmail.com>
2025-12-24 14:32:38 +0200

    Dynamically allocate memory with malloc and new

--- C-and-C++/structs-classes/destructor.cpp
+++ C-and-C++/structs-classes/destructor.cpp
@@ -4,20 +4,30 @@ using namespace std;
 
 class Number {
   public:
-    Number(double n) : number(n) {
-      cout << "Constructor executing!\n" << "Number: " << number << endl;
+    Number(double n) {
+      number = (double *) malloc(sizeof(double)); // C implemenation
+      number = new double;
+      *number = n;
+      cout << "Constructor executing!\n" << "Number: " << *number << endl;
     }
     ~Number() {
-      cout << "Destructor executing!\n" << "Number: " << number << endl;
+      cout << "Destructor executing!\n" << "Number: " << *number << endl;
+      free(number);
     }
   private:
-    double number;
+    double *number;
 };
+
+void test() {
+  Number six(6);
+}
 
 int main() {
 
+  test();
+
   Number Eight(8);
-  
+
 
   return 0;
 }
