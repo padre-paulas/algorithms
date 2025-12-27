36e39f77582f6b0e53c633e297ea169dda6677a1
padre_paulas <pounowyou@gmail.com>
2025-12-27 17:06:12 +0200

    add function that calculated difference between max and min arr elements

--- C-and-C++/experiments/exam-exp.cpp
+++ C-and-C++/experiments/exam-exp.cpp
@@ -10,6 +10,8 @@ struct Book {
 int f7(char []);
 void tellSize();
 void doStruct();
+int calculateArr(int *arr, int size);
+
 
 int main() {
 
@@ -18,18 +20,24 @@ int main() {
 
   // std::cout << (m = a > b ? a : b);
 
-  int a = 1, b = 2;
-  int *p = &a;
-  int *q = &b;
-  a = b;
+  // int a = 1, b = 2;
+  // int *p = &a;
+  // int *q = &b;
+  // a = b;
   
-  std::cout << (*p == *q) << '\n';
+  // std::cout << (*p == *q) << '\n';
 
-  char c[10] = "543";
+  // char c[10] = "543";
 
-  std::cout << f7(c) << std::endl;
+  // std::cout << f7(c) << std::endl;
 
-  tellSize();
+  // tellSize();
+
+  // doStruct();
+
+  int arr[10] = {3, 5, 4, 2, 7, 87, 23, 8, 22, 11};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  std::cout << calculateArr(arr, size) << std::endl;
 
   return 0;
 }
@@ -62,5 +70,28 @@ void tellSize() {
 }
 
 void doStruct() {
- 
+  std::cout << sizeof(Book) << std::endl;
+  std::cout << sizeof(int) << std::endl;
+  std::cout << sizeof(double) << std::endl;
+
+  if (2 % 2) std::cout << "AAAA\n";
+}
+
+int calculateArr(int *arr, int size) {
+  int max = INT_MIN;
+  int min = INT_MAX;
+  bool even = false;
+
+  for (int i = 0; i < size; i++) {
+    if (!even) {
+      if (arr[i] > max) max = arr[i];
+    } else {
+      if (arr[i] < min) min = arr[i];
+    }
+    even = !even;
+  }
+  std::cout << min << std::endl;
+  std::cout << max << std::endl;
+
+  return max - min;
 }
