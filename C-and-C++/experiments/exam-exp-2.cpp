6f241f2926b84e20590d838c520641a51775990b
padre_paulas <pounowyou@gmail.com>
2026-01-04 17:49:49 +0200

    check q2

--- C-and-C++/experiments/exam-exp-2.cpp
+++ C-and-C++/experiments/exam-exp-2.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void q2();
+
 int main() {
 
-  int a = 15, b = 0, c;
+  // int a = 15, b = 0, c;
 
-  if (a = b) { c = b; c = a; } c = b;
+  // if (a = b) { c = b; c = a; } c = b;
 
-  cout << c << endl;
+  // cout << c << endl;
+
+  q2();
 
   return 0;
+}
+
+void q2() {
+  double d1 = 2.56, d2, d3;
+  d2 = (int)d1 + 1.5;
+  d3 = (int)(d1 + 1.5);
+
+  cout << "d2: " << d2 << " d3: " << d3 << endl;
 }
