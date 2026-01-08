59fdc97512ae6fb36420824ba64cfa49455e8a6c
padre_paulas <pounowyou@gmail.com>
2026-01-08 13:35:18 +0200

    create exam practice 3 file

---
+++ C-and-C++/experiments/exam-exp-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+
+
+
+int main() {
+
+  unsigned short int x = 5;
+  long int y = 6;
+  unsigned short *ptr1 = &x;
+  long *prt2 = &y;
+  // if (ptr1 < prt2); 
+
+
+  return 0;
+}
