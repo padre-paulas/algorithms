0f55711fd195600cbca0a9524a10e7fe9e50cf07
padre_paulas <pounowyou@gmail.com>
2026-01-09 15:54:28 +0200

    Update exam algorithm to include input arr function

--- C-and-C++/experiments/exam.cpp
+++ C-and-C++/experiments/exam.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 
-// int* inputArr();
+void inputArr();
 void sort(int arr[], int length);
 void showArr(int arr[], int length);
 
 int main() {
 
-  // inputArr();
-  int arr[] = {3, 5, -3, 90, 6, 7, 45, 888};
-  int length = sizeof(arr) / sizeof(int);
-
-  showArr(arr, length);
-  sort(arr, length);
-  showArr(arr, length);
+  inputArr();
 
   return 0;
 }
 
-// int inputArr() {
-//   int length;
-//   std::cout << "Hi! How many elements should the array contain? ";
-//   std::cin >> length;
+void inputArr() {
+  int length;
+  std::cout << "Hi! How many elements should the array contain? ";
+  std::cin >> length;
 
-//   int arr[length];
+  int arr[length];
 
-//   for (int i = 0; i < length; i++) {
-//     std::cout << "Enter an int: ";
-//     std::cin >> arr[i];
-//   }
+  for (int i = 0; i < length; i++) {
+    std::cout << "Enter an int: ";
+    std::cin >> arr[i];
+  }
 
-//   std::cout << "Here's your array: ";
-//   for (int i = 0; i < length; i++) {
-//     std::cout << arr[i] << "  ";
-//   }
-//   std::cout << std::endl;
-//   return arr;
-// }
+  showArr(arr, length);
+  sort(arr, length);
+  showArr(arr, length);
+}
 
 void sort(int arr[], int length) {
   int temp = 0;
