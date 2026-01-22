38218395a3de28879a90807686977f7235ed6f17
padre_paulas <pounowyou@gmail.com>
2026-01-22 22:39:10 +0200

    add alternative selection sort implementation

--- grokking/JS/selection-sort.js
+++ grokking/JS/selection-sort.js
@@ -1,9 +1,9 @@
 const arr = [16, 2, 3, 4, 11, 5, 7, 90, 8];
-let arrCopy = [];
 
 function selection(arr) {
   let value = Infinity;
   let prevValue = -Infinity;
+  let arrCopy = [];
 
   for (let i = 0; i < arr.length; i++) {
     value = Infinity;
@@ -16,7 +16,37 @@ function selection(arr) {
     arrCopy.push(value);
     prevValue = value;
   }
+  return arrCopy;
 }
 
-selection(arr);
-console.log(arrCopy);
+// let arrCopy = selection(arr);
+// console.log(arrCopy);
+
+function selectMin(arr) {
+  let min = arr[0];
+  let minIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+      minIndex = i;
+    }
+  }
+  return minIndex;
+}
+
+function selection2_0(arr) {
+  let arrCopy = [];
+  let minIndex;
+  let length = arr.length
+  
+  for (let i = 0; i < length; i++) {
+    minIndex = selectMin(arr);
+    arrCopy.push(arr[minIndex]);
+    arr.splice(minIndex, 1)
+  }
+  return arrCopy
+}
+
+let arrCopy2 = selection2_0(arr);
+console.log(arrCopy2);
+
