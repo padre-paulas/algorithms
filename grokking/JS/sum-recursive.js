c64f4475bee8a2f69046ac92c7a8196e605cd6e8
padre_paulas <pounowyou@gmail.com>
2026-01-24 13:25:31 +0200

    create recursive sum function for array

---
+++ grokking/JS/sum-recursive.js
@@ -0,0 +1,15 @@
+const arr = [1, 3, 5, 77, 4];
+let sum = 0;
+
+function addArrElements(arr, count) {
+  if (count == 0) return 0;
+  else {
+    count--;
+    console.log(arr[count]);
+    return arr[count] + addArrElements(arr, count);
+  }
+}
+
+console.log("hello " + addArrElements(arr, 5));
+
+
